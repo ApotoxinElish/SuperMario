7afc9dcce989264876d28c4d7d45175de0d1c564
ApotoxinElish <19079181d@connect.polyu.hk>
2021-10-07 02:19:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from source.states import main_menu, load_screen, level
 def main():
 
     state_dict = {
-        'main_menu': main_menu.MainMenu(),
-        'load_screen': load_screen.LoadScreen(),
-        'level': level.Level(),
-        'game_over': load_screen.GameOver()
+        "main_menu": main_menu.MainMenu(),
+        "load_screen": load_screen.LoadScreen(),
+        "level": level.Level(),
+        "game_over": load_screen.GameOver(),
     }
-    game = tools.Game(state_dict, 'main_menu')
+    game = tools.Game(state_dict, "main_menu")
     game.run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
